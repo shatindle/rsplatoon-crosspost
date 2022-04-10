1238abbe64b1e464d327c5706cc7c9744a38a979
Shane Tindle <sha.tindle@gmail.com>
2022-04-09 22:58:29 -0500

    Added support for weapons translations

--- DAL/japaneseToEnglishSplatoonApi.js
+++ DAL/japaneseToEnglishSplatoonApi.js
@@ -1,12 +1,24 @@
 const stages = require("./langageData/stages.json");
+const weapons = require("./langageData/weapons.json").weapons;
 
 function stageSwap(text) {
-    for (const [game, list] of Object.entries(stages)) {
-        for (const [japanese, english] of Object.entries(list)) {
+    // stage transformation
+    for (var [game, list] of Object.entries(stages)) {
+        for (var [japanese, english] of Object.entries(list)) {
             try {
                 text = text.replace(japanese, english);
             } catch { /* don't care */}
         }
+    }
+
+    // weapon transformation
+    for (var weaponPair of weapons) {
+        var japanese = weaponPair[0];
+        var english = weaponPair[1];
+
+        try {
+            text = text.replace(japanese, english);
+        } catch { /* don't care */}
     }
 
     return text;
