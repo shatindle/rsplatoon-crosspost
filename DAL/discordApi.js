f85dfb5cfb29d877f196782247a9f11998a3b9b6
Shane Tindle <sha.tindle@gmail.com>
2023-05-26 16:02:29 -0500

    Added guild members intent

--- DAL/discordApi.js
+++ DAL/discordApi.js
@@ -6,6 +6,7 @@ const discord = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MEMBERS,
         Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ], 
     partials: ['MESSAGE', 'CHANNEL', 'REACTION'] 
